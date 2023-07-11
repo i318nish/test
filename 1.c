5a60bb4e9ab6eb29f65831265f3003c9276d063c
i318nish <138637812+i318nish@users.noreply.github.com>
2023-07-12 03:35:45 +0900

    n & 1 の計算を優先して行うように括弧を追加した

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+
+  printf("n = ");
+  scanf("%d", &n);
+
+  if( (n & 1) == 0 ){ //(n & 1 == 0 )から((n & 1) == 0)に変更、 n & 1 の計算を優先して行うように括弧を追加した
+    printf("%d は偶数です\n", n);
+  }
+  return 0;
+}
